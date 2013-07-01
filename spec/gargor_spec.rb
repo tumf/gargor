88e8d9fff621513d1958f3987e3089280969f3a5
Yoshihiro Takahara <y.takahara@gmail.com>
2013-07-02 08:17:35 +0900

    add tests

--- spec/gargor_spec.rb
+++ spec/gargor_spec.rb
@@ -96,10 +96,69 @@ describe Gargor,".selection" do
     Gargor.load_dsl("dummy")
     a = Gargor.mutate; a.fitness = 0.4
     b = Gargor.mutate; b.fitness = 0.6
-    g = [a,b]
+    g = Gargor::Individuals.new
+    g << a << b
     expect(Gargor.selection g).to be_kind_of Gargor::Individual
   end
 end
 
 
+describe Gargor, ".validate" do
+  it "raise Gargor::GargorError unless population > 0" do
+    Gargor.start
+    expect{
+      Gargor.validate 
+    }.to raise_error Gargor::ValidationError
+  end
+end
 
+describe Gargor, ".first_generation?" do
+  it "returns true if not .next_generation called" do
+    Gargor.start
+    expect(Gargor.first_generation?).to be true
+    Gargor.next_generation
+    expect(Gargor.first_generation?).to be false
+  end
+end
+
+describe Gargor, ".prev_count" do
+  it "returns previous generation count" do
+    g = Gargor::Individuals.new
+    a = Gargor.mutate; a.fitness = 0.4
+    g << a
+    expect(Gargor.prev_count(g)).to be 1
+
+    b = Gargor.mutate; b.fitness = 0 # not fit for this environment
+    c = Gargor.mutate; c.fitness = 0.6
+    g << b << c
+    expect(Gargor.prev_count(g)).to be 2
+  end
+end
+
+describe Gargor, ".select_elites" do
+  it "returns Gargor::Individuals" do
+    g = Gargor::Individuals.new
+    3.times { g << Gargor.mutate }
+
+    Gargor.start
+    gg = Gargor.select_elites(g,2)
+    expect(gg).to be_kind_of Gargor::Individuals
+    expect(gg.count).to be 2
+  end
+
+  it "returns count of elites" do
+    g = Gargor::Individuals.new
+    3.times { g << Gargor.mutate }
+    Gargor.start
+    expect(Gargor.select_elites(g,3).count).to be 3
+    expect(Gargor.select_elites(g,4).count).to be 3
+  end
+end
+
+describe Gargor, ".mutaion?" do
+  it "returns true by mutation probability" do
+    Gargor.stub(:rand).and_return(0.05,0.15)
+    expect(Gargor.mutation?(0.1)).to be true
+    expect(Gargor.mutation?(0.1)).to be false
+  end
+end
