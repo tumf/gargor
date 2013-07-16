4cd61d645f854132ad11486f34f3166cbc422c26
Yoshihiro TAKAHARA <y.takahara@gmail.com>
2013-07-16 12:03:04 +0900

    add reporter_spec

---
+++ spec/gargor_reporter_spec.rb
@@ -0,0 +1,16 @@
+require 'helper'
+require 'gargor/reporter'
+
+describe Gargor::OptimizeReporter, ".table" do
+  before do
+    to_load_fixture "sample-1.rb"
+    Gargor.start
+    Gargor.load_dsl("dummy")
+    @a,@b = Gargor.mutate,Gargor.mutate
+  end
+
+  it "return Terminal::Table object" do
+    expect(Gargor::OptimizeReporter.table(@a,@b)).to be_kind_of Terminal::Table
+  end
+
+end
