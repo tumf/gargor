805406e63e2fe3df725d50e107d9ceb27716339e
Yoshihiro TAKAHARA <y.takahara@gmail.com>
2013-07-16 11:46:29 +0900

    fix: spec

--- spec/gargor_cli_spec.rb
+++ spec/gargor_cli_spec.rb
@@ -4,7 +4,7 @@ require 'gargor/cli'
 describe Gargor::CLI,".start" do
   it "exits when raise ExterminationError" do
     to_load_fixture "sample-1.rb"
-    Gargor.stub(:start) { 
+    Gargor.stub(:populate) { 
       raise Gargor::ExterminationError
     }
     expect{Gargor::CLI.start([])}.to raise_error(SystemExit)
